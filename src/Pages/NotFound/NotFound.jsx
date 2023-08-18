82d3202f6c92fe199981f2c3adddca215930f9ec
Lorelei <loreleimoutiez@gmail.com>
2023-08-18 09:43:28 +0200

    feat/error_page

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,3 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export function NotFound() {
-    return <h1>Erreur</h1>
+    return (
+        <main className='App'>
+            <h1 className='error_msg'>404</h1>
+            <h2>Oups! La page que vous demandez n'existe pas.</h2>
+            <p><Link to="/">Retourner sur la page d’accueil</Link></p>
+        </main>
+    )
 }
